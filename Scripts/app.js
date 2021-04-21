823cafaf896b1c806dca398521d79a66b88ebede
ayiu93 <anthonyyiu93@gmail.com>
2021-04-21 14:04:15 -0700

    added images on load

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,6 @@
 function update_image() {
     for(i=1;i<5;i++) {
-        console.log(i)
-       let img = document.querySelector(`'id=Album ${i}'`).setAttribute("src",`'images\\Album${i}.png'`)
+        document.querySelector(`[id="Album\ ${i}"]`).src = `Images\\Album${i}.png`
     }
 }
 
