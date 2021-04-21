9b4a9a1ac6a0195c7811dcb34c43a7119a8fb242
ayiu93 <anthonyyiu93@gmail.com>
2021-04-21 14:08:49 -0700

    working on cart function

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,18 @@
-function update_image() {
+const albums = [
+    { name: "Album 1", price: 12.99, inCart: false }, 
+    { name: "Album 2", price: 14.99, inCart: false }, 
+    { name: "Album 3", price: 9.99, inCart: false }, 
+    { name: "Album 4", price: 19.99, inCart: false }
+  ];
+
+function updateImage() {
     for(i=1;i<5;i++) {
-        document.querySelector(`[id="Album\ ${i}"]`).src = `Images\\Album${i}.png`
+        document.querySelector(`[id="Album\ ${i}"]`).src = `Images\\Album${i}.png`;
     }
 }
 
-window.addEventListener('load', update_image())
+function addToCart() {
+
+}
+
+window.addEventListener('load', updateImage());
